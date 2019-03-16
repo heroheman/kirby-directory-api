9646a7eaa17a524250a805e2e46a006a5fb9ae7c
Florenz Heldermann <hi@flore.nz>
2019-03-16 15:57:06 +0100

    setup new endpoint
    * add lowdb
    * add testendpoint
    * fetch data with axios
    * save data to lowdb
    * add node-cron (not implemented)
    * add esm
    * add nodemon
    * add devtask in npm

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const fs = require('fs');
+import db from './lib/db';
+
+import { runCronFetch } from './lib/getItems.js';
 
 const app = express();
 app.use(helmet());
@@ -11,25 +14,30 @@ const port = 63473;
 
 const server = app.listen(port, () => {
   console.log('Express server ready to serve. Port: ' + port + '. Bye.');
+  runCronFetch();
 });
 
 server.timeout = 1000 * 60 * 10; // 10 minutes
 
 // Use middleware to set the default Content-Type
 app.use(function (req, res, next) {
-    res.header('Content-Type', 'application/json');
-    next();
+  res.header('Content-Type', 'application/json');
+  next();
+});
+
+app.get('/themesTest', (req, res) => {
+  const themes = db.get('themes');
+  res.send(themes);
 });
 
 app.get('/themes', (req, res) => {
-    fs.readFile('./json/themes.json', 'utf8', (err, data) => {
-        res.send(data)
-    })
-})
+  fs.readFile('./json/themes.json', 'utf8', (err, data) => {
+    res.send(data);
+  });
+});
 
 app.get('/plugins', (req, res) => {
-    fs.readFile('./json/plugins.json', 'utf8', (err, data) => {
-        res.send(data)
-    })
-})
-
+  fs.readFile('./json/plugins.json', 'utf8', (err, data) => {
+    res.send(data);
+  });
+});
